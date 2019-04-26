7fac9fca13c06badb8fcbc2a1064461248f32cd6
Jared <jared.c.lee@wsu.edu>
2019-04-26 15:27:43 -0700

    Made several changes, but trying to fix 'Return to main' modal dismiss.

--- js/doGet.js
+++ js/doGet.js
@@ -42,22 +42,21 @@ NOTES:      Authenticates inputed email and password.
 ****************************************************************/
 
 function authenticate(email, password) {
-    // How to get by sheet name: https://developers.google.com/apps-script/reference/spreadsheet/spreadsheet#getSheetByName(String)
     var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Login Info");
     var numUsers = sheet.getLastRow();
-    if(numUsers == 0)
-        return 0;
-
+    if(numUsers == 0) {
+      return 0;
+    }
+        
     password = Utilities.base64Encode(Utilities.computeDigest(Utilities.DigestAlgorithm.SHA_512, password));
     
     var users = sheet.getSheetValues(1, 1, numUsers, 2);
-    for(var i = 0; i < users.length; i++)
-    {
-        if(users[i][0] == email && users[i][1] == password)
-        {
+    for(var i = 0; i < users.length; i++) {
+        if(users[i][0] == email && users[i][1] == password) {
             return 1;
         }
     }
+
     return 0;
 }
 
@@ -77,9 +76,10 @@ function requestAccount(email, password) {
 
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Email Whitelist");
   var numEmails = sheet.getLastRow();
-  if(numEmails == 0)
-      return 0;
-  
+  if(numEmails == 0) {
+    return 0;
+  }
+      
   var emails = sheet.getSheetValues(1, 1, numEmails, 1);
   for(var i = 0; i < emails.length; i++) {
     if(email == emails[i][0]) {
@@ -121,41 +121,28 @@ NOTES:      Gets work order and phase codes from spreadsheet
 ****************************************************************/
 
 function loadFromSpreadsheet() {
-    var workOrderSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Work Orders");
-    var phaseCodeSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Phase Codes");
+  var workOrderSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Work Orders");
+  var phaseCodeSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Phase Codes");
 
-    var numWorkOrders = workOrderSheet.getLastRow() - 1;
-    var numPhaseCodes = phaseCodeSheet.getLastRow() - 1;
+  var numWorkOrders = workOrderSheet.getLastRow() - 1;
+  var numPhaseCodes = phaseCodeSheet.getLastRow() - 1;
 
-    var workOrders;
-    var phaseCodes;
-    
-    workOrders = workOrderSheet.getSheetValues(2, 1, numWorkOrders, 2);
-    phaseCodes = phaseCodeSheet.getSheetValues(2, 1, numPhaseCodes, 2);
+  var workOrders;
+  var phaseCodes;
+  
+  workOrders = workOrderSheet.getSheetValues(2, 1, numWorkOrders, 2);
+  phaseCodes = phaseCodeSheet.getSheetValues(2, 1, numPhaseCodes, 2);
 
-    return [workOrders, phaseCodes];
+  return [workOrders, phaseCodes];
 }
 
-/****************************************************************
-
-FUNCTION:   submitEmployeeInfo()
-
-ARGUMENTS:  None
-
-RETURNS:    None
-
-NOTES:      Inputs employee info to the spreadsheet
-
-****************************************************************/
-
-function submitEmployeeInfo(email, allEmployeeInfo) {
-  var nameCell, classificationCell, jobCell, weekEndingCell;
+function submitEmployeeInfo(email) {
   var date = new Date();
-  var blankTimecard = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Blank Timecard");
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var blankTimecard = ss.getSheetByName("Blank Timecard");
+  ss.insertSheet(email + " - " + date.toLocaleString(), {template: blankTimecard});
 
-  var newTimecard = blankTimecard.copy(email + " - " + date.toLocaleString());
+  var nameCell = ss.getRange('B8');
+  //nameCell.setValue(employeeInfo[0]);
 
-  for (var i = 0; i < allEmployeeInfo.length; i++) {
-
-  }
 }
